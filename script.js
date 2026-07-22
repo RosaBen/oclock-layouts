1eaaaece17d24f4bb819c0ebcee82744c0f0f124
RosaBen <benchabane.rosa@outlook.com>
2026-07-22 18:24:16 +0200

    added javascript for list of cats

--- script.js
+++ script.js
@@ -39,4 +39,15 @@ const actors = [{
 }
 ];
 
+const ul = document.querySelector(".cast ul");
 
+actors.forEach(element => {
+  const li = document.createElement("li");
+  const link = document.createElement("a");
+  link.textContent = element.name;
+  link.href = element.url;
+  link.target = "_blank";
+  link.rel = "noopener noreferrer";
+  li.appendChild(link);
+  ul.appendChild(li);
+});
